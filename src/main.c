070f4c7f6eb82dfbbdc6bb2c08b9fca78be45b7b
albertgoncalves <goncalves.albert@gmail.com>
2021-01-02 22:42:59 -0500

    impl basic function call

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 /* NOTE: Useful resources:
  * `https://bernsteinbear.com/blog/lisp/`
  * `http://cs.brown.edu/courses/cs033/docs/guides/x64_cheatsheet.pdf`
+ * `http://6.s081.scripts.mit.edu/sp18/x86-64-architecture-guide.html`
+ * `https://www.felixcloutier.com/x86/call`
  * `https://software.intel.com/content/www/us/en/develop/articles/intel-sdm.html`
  */
 
@@ -42,12 +44,15 @@ typedef i32 (*FnVoidI32)(void);
         memory->buffer_index += sizeof(type);                                \
     }
 
-EMIT_1_BYTE(emit_mov_ebx_imm32, 0xBB)
+EMIT_1_BYTE(emit_mov_eax_imm32, 0xB8)
+EMIT_1_BYTE(emit_mov_edi_imm32, 0xBF)
 EMIT_1_BYTE(emit_push_rbx, 0x53)
 EMIT_1_BYTE(emit_pop_rbx, 0x5B)
+EMIT_1_BYTE(emit_call, 0xE8)
 EMIT_1_BYTE(emit_ret, 0xC3)
 EMIT_2_BYTES(emit_mov_eax_ebx, 0x89, 0xD8)
-EMIT_1_VAR(emit_i32, i32)
+EMIT_2_BYTES(emit_mov_ebx_edi, 0x89, 0xFB)
+EMIT_1_VAR(emit_i32, i32) // NOTE: 4 bytes!
 
 static Program transform(Memory* memory) {
     Program program = {0};
@@ -75,13 +80,35 @@ i32 main(void) {
     EXIT_IF(!memory);
     i32 x = 42;
     {
-        emit_push_rbx(memory);
-        emit_mov_ebx_imm32(memory);
-        emit_i32(memory, x);
-        emit_mov_eax_ebx(memory);
-        emit_pop_rbx(memory);
-        emit_ret(memory);
+        /*     - 0-     mov     edi, `x`
+         *     - 5-     call    label
+         *     -10-     ret
+         *     -11-     mov     eax, `x + 1` ; NOTE: Never evaluated!
+         */
+        emit_mov_edi_imm32(memory); //  0 + 1 ->  1
+        emit_i32(memory, x);        //  1 + 4 ->  5
+        emit_call(memory);          //  5 + 1 ->  6
+        emit_i32(memory, 6);        //  6 + 4 -> 10
+        emit_ret(memory);           // 10 + 1 -> 11
+        emit_mov_eax_imm32(memory); // 11 + 1 -> 12
+        emit_i32(memory, x + 1);    // 12 + 4 -> 16
     }
+    EXIT_IF(memory->buffer_index != 16);
+    {
+        /*          label:
+         *     -16-     push    rbx
+         *     -17-     mov     ebx, edi
+         *     -19-     mov     eax, ebx
+         *     -21-     pop     rbx
+         *     -22-     ret
+         */
+        emit_push_rbx(memory);    // 16 + 1 -> 17
+        emit_mov_ebx_edi(memory); // 17 + 2 -> 19
+        emit_mov_eax_ebx(memory); // 19 + 2 -> 21
+        emit_pop_rbx(memory);     // 21 + 1 -> 22
+        emit_ret(memory);         // 22 + 1 -> 23
+    }
+    EXIT_IF(memory->buffer_index != 23);
     {
         Program program = transform(memory);
         {
